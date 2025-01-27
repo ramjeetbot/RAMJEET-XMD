52b0c585281b4705d77150885d4d52202a0fa6a8
BABA RAMJEET <pain39582@gmail.com>
2025-01-27 12:49:09 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -26,7 +26,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
         // Send the status message with an image
         await conn.sendMessage(from, { 
-            image: { url: `https://qu.ax/oBgGR.jpg` },  // Image URL
+            image: { url: `https://qu.ax/kfoYM.jpg` },  // Image URL
             caption: status,
             contextInfo: {
                 mentionedJid: [m.sender],
